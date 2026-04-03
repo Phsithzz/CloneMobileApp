09b27fbc2f593d161f044b7c880f743cf88689cf
Phsithzz <phoosith2548@gmail.com>
2026-04-03 21:13:51 +0700

    remove AppBar and move layout to SafeArea

--- lib/screens/invite_page.dart
+++ lib/screens/invite_page.dart
@@ -7,107 +7,114 @@ class InvitePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        actions: [
-          Image.asset("assets/images/language.jpg", width: 30, height: 30),
-          Icon(Icons.keyboard_arrow_down, color: Colors.blueGrey[300]),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
 
-          children: [
-            Image.asset(
-              "assets/images/logo.jpg",
-              width: 200,
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-
-            const Text(
-              "Please fill in the Invitation Code Provided by your \n administrator to access empeo within your company.",
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 36),
-
-            SizedBox(
-              width: double.infinity,
-              height: 40,
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: "Invitation Code",
-                  hintStyle: TextStyle(fontSize: 14, color: Colors.grey[400]),
-
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(6),
-                    borderSide: const BorderSide(
-                      color: Color.fromARGB(255, 209, 208, 208),
-                      width: 1.5,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+        
+            children: [
+               Align(
+                alignment: Alignment.topRight,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset("assets/images/language.jpg", width: 30),
+                    Icon(Icons.keyboard_arrow_down, color: Colors.grey[300]),
+                  ],
+                ),
+              ),
+              Image.asset(
+                "assets/images/logo.jpg",
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+        
+              const Text(
+                "Please fill in the Invitation Code Provided by your \n administrator to access empeo within your company.",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 36),
+        
+              SizedBox(
+                width: double.infinity,
+                height: 40,
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Invitation Code",
+                    hintStyle: TextStyle(fontSize: 14, color: Colors.grey[400]),
+        
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 209, 208, 208),
+                        width: 1.5,
+                      ),
                     ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(6),
-                    borderSide: const BorderSide(
-                      color: Color.fromARGB(255, 209, 208, 208),
-                      width: 1.5,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 209, 208, 208),
+                        width: 1.5,
+                      ),
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(6),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF45B26),
-                      width: 2,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF45B26),
+                        width: 2,
+                      ),
                     ),
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 14,
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 14,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 24),
-
-            SizedBox(
-              width: double.infinity,
-              height: 42,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xffF45B26),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+              const SizedBox(height: 24),
+        
+              SizedBox(
+                width: double.infinity,
+                height: 42,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffF45B26),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    elevation: 0,
                   ),
-                  elevation: 0,
+                  child: Text(
+                    "Confirm",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
+              ),
+              const SizedBox(height: 8),
+              TextButton(
+                onPressed: () {},
                 child: Text(
-                  "Confirm",
+                  "Skip",
                   style: TextStyle(
-                    color: Colors.white,
                     fontSize: 14,
+                    color: const Color(0xffF45B26),
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 8),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "Skip",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: const Color(0xffF45B26),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
