d4b5f8deb306548fc606a247de21206949df9067
Phsithzz <phoosith2548@gmail.com>
2026-04-29 22:19:55 +0700

    refactor:create function ontap

--- lib/screens/bottom_nav_page.dart
+++ lib/screens/bottom_nav_page.dart
@@ -12,26 +12,36 @@ class BottomNavPage extends StatefulWidget {
 class _BottomNavPageState extends State<BottomNavPage> {
   int _currentIndex = 3;
 
-  final List<Widget> _pages = [
-    const Center(child: Text('Home Page')),
-    const CalendarPage(),
-    const Center(child: Text("Group Page")),
-    const ChatPage(),
-    const Center(child: Text('Notifications Page')),
-    const Center(child: Text('Profile Page')),
+  static const List<Widget> _pages = [
+     Center(child: Text('Home Page')),
+     CalendarPage(),
+     Center(child: Text("Group Page")),
+     ChatPage(),
+     Center(child: Text('Notifications Page')),
+     Center(child: Text('Profile Page')),
   ];
+
+  void _onTabTapped(int index) {
+  setState(() {
+    _currentIndex = index;
+  });
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
+      body: SafeArea(
+        child: _pages[_currentIndex]),
+
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
+        onTap: _onTabTapped,
         type: BottomNavigationBarType.fixed,
         selectedItemColor: Theme.of(context).primaryColor,
         unselectedItemColor: Colors.grey,
         showSelectedLabels: false,
         showUnselectedLabels: false,
+
         items: [
           const BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
@@ -49,23 +59,30 @@ class _BottomNavPageState extends State<BottomNavPage> {
             icon: Icon(Icons.perm_contact_calendar_rounded),
             label: '',
           ),
+
           BottomNavigationBarItem(
             icon: Stack(
               children: [
+
                 const Icon(Icons.fact_check_outlined),
+
                 Positioned(
                   right: 0,
                   top: 0,
+
                   child: Container(
                     padding: const EdgeInsets.all(2),
+
                     decoration: const BoxDecoration(
                       color: Colors.red,
                       shape: BoxShape.circle,
                     ),
+
                     constraints: const BoxConstraints(
                       minWidth: 14,
                       minHeight: 14,
                     ),
+
                     child: const Text(
                       '99+',
                       style: TextStyle(color: Colors.white, fontSize: 8),
