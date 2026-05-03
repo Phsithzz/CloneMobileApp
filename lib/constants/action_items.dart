bb3a8b77831de0a0535df51ef89e3521ccb2cc0d
Phsithzz <phsithzz@decepticon.local>
2026-05-03 22:59:51 +0700

    feature:change icon

--- lib/constants/action_items.dart
+++ lib/constants/action_items.dart
@@ -4,12 +4,12 @@ class ActionItems {
   ActionItems._();
 
   static const List<(String, IconData, Color)> items = [
-    ('ลางาน', Icons.beach_access_outlined, Color(0xFFD84315)),
-    ('ปฏิบัติงานนอกสถานที่', Icons.map_outlined, Color(0xFFD84315)),
-    ('ล่วงเวลา', Icons.access_time, Color(0xFFD84315)),
-    ('รับรองเวลา', Icons.person_add_outlined, Color(0xFF78909C)),
-    ('ขอสะสมเวลา', Icons.person_remove_outlined, Color(0xFF78909C)),
-    ('แลกเวลา', Icons.swap_horiz, Color(0xFF78909C)),
-    ('ค่าใช้จ่าย', Icons.attach_money, Color(0xFF78909C)),
+    ('ลางาน', Icons.event_busy, Color(0xffF45B26)),
+    ('ปฏิบัติงานนอกสถานที่', Icons.location_on, Color(0xffF45B26)),
+    ('ล่วงเวลา', Icons.access_time_filled, Color(0xffF45B26)),
+    ('รับรองเวลา', Icons.check_circle, Color(0xffF45B26)),
+    ('ขอสะสมเวลา', Icons.add_circle, Color(0xffF45B26)),
+    ('แลกเวลา', Icons.swap_horiz, Color(0xffF45B26)),
+    ('ค่าใช้จ่าย', Icons.attach_money, Color(0xffF45B26)),
   ];
 }
